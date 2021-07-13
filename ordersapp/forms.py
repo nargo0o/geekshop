927e3f83453359603e36b1ea38861faa0e8c392e
Nargo <nargo4535@gmail.com>
2021-07-13 18:29:08 +0300

    lesson_5. Dynamic formset

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -25,3 +25,16 @@ class OrderItemEditForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
+
+
+class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label="цена", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+    class Meta:
+        model = OrderItem
+        exclude = ()
